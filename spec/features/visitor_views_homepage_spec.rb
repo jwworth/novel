4f23084496a9de70c3ce655ac1239c5d7edded4d
Jake Worth <jakeworth82@gmail.com>
2015-09-11 15:55:50 -0500

    Round out test

--- spec/features/visitor_views_homepage_spec.rb
+++ spec/features/visitor_views_homepage_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 describe 'Visitor views homepage', type: :feature do
-  it 'and sees chapters' do
-    FactoryGirl.create_list(:chapter, 3)
-    visit root_path
+  context 'with chapters' do
+    it 'and sees chapters' do
+      FactoryGirl.create_list(:chapter, 3)
+      visit root_path
 
-    expect(page).to have_selector('.chapter', count: 3)
+      expect(page).to have_selector('.chapter', count: 3)
+      expect(page).to have_selector('.body', count: 3)
+      expect(page).to have_selector('.title', count: 3)
+    end
   end
 
-  it 'and sees a well-designed blank page' do
-    visit root_path
+  context 'with no chapters' do
+    it 'and sees a well-designed blank page' do
+      visit root_path
 
-    expect(page).to have_content('No chapters.')
-    expect(page).to_not have_selector('.chapter', count: 3)
+      expect(page).to have_content('No chapters.')
+      expect(page).to_not have_selector('.chapter')
+      expect(page).to_not have_selector('.body')
+      expect(page).to_not have_selector('.title')
+    end
   end
 end
