8bcec3477561fa72fb1f49dc26e80bef887a5ab8
Jake Worth <jakeworth82@gmail.com>
2015-11-05 07:42:33 -0600

    Don't strip noise; the files have been prepared

--- config/initializers/markov.rb
+++ config/initializers/markov.rb
@@ -1,11 +1,7 @@
 string = if Rails.env.production?
-  Dir['lib/assets/books/*.txt'].map { |f| File.read(f) }.join
+  Dir['lib/assets/books/*.txt'].map { |f| File.read(f) }.join(' ')
 else
   File.read(Rails.root.join('lib', 'assets', 'books', 'siddhartha.txt'))
 end
-
-# Strip Project Gutenberg-specific noise
-string.gsub!(/^[A-Z\s]+$/, '')
-string.gsub!(/http\S+/, '')
 
 MARKOV = Remarkovable.new(string: string)
