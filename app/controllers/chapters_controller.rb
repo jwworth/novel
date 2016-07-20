49115479c21c527de2f99b33baf0040dfd1584f9
Jake Worth <jake@jakeworth.com>
2016-07-19 19:59:59 -0500

    Add pagination

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,20 +1,11 @@
 class ChaptersController < ApplicationController
   def index
     @chapters = Chapter.order(created_at: :desc)
+      .page(params[:page])
+      .per(20)
   end
 
   def show
     @chapter = Chapter.find(params[:id])
-  end
-
-  def create
-    @chapter = Chapter.new(chapter_params)
-    redirect_to root_path if @chapter.save
-  end
-
-  private
-
-  def chapter_params
-    params.require(:chapter).permit(:title, :body)
   end
 end
